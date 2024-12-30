2ef6f6df87f536dc68551729aec642c399239805
vismodeb <vismodeb2000@gmail.com>
2024-12-30 11:59:29 +0600

    navbar menu mobile design done

--- client-site/src/components/shared/navbar/Navbar.jsx
+++ client-site/src/components/shared/navbar/Navbar.jsx
@@ -1,13 +1,13 @@
-import { IoIosEye, IoIosEyeOff, IoMdMenu } from "react-icons/io";
+import { IoIosEye, IoIosEyeOff } from "react-icons/io";
 import { Link } from "react-router-dom";
 import AccoundModal from "../modal/AccoundModal";
 import { useState } from "react";
 import { IoClose } from "react-icons/io5";
-import { HiOutlineLogin } from "react-icons/hi";
-import { BiUserPlus } from "react-icons/bi";
-import { MdHome } from "react-icons/md";
 import { FaSync } from "react-icons/fa";
 import { TbCurrencyTaka } from "react-icons/tb";
+import { BiMenuAltLeft } from "react-icons/bi";
+import { LuHardDriveDownload } from "react-icons/lu";
+import { MdOutlineMessage } from "react-icons/md";
 
 const Navbar = ({ open }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -16,13 +16,11 @@ const Navbar = ({ open }) => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
-  const closeSidebar = () => {
-    setIsSidebarOpen(false);
-  };
-
   const openModal = (id) => {
     document.getElementById(id).showModal();
   };
+
+  // LanguageModal
   const [isLanguageModalOpen, setIsLanguageModalOpen] = useState(false);
   const toggleLanguageModal = () => {
     setIsLanguageModalOpen(!isLanguageModalOpen);
@@ -55,24 +53,34 @@ const Navbar = ({ open }) => {
   return (
     <div>
       <div
-        className={`bg-[#222222] p-4 fixed left-0 right-0 z-20 duration-300 ${
+        className={`bg-[#222222] py-2 md:py-4 px-4 fixed left-0 right-0 z-20 duration-300 ${
           !open ? "md:ml-16" : "md:ml-64"
         }`}
       >
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-1 sm:gap-3">
-            <div className="md:hidden text-yellow-300" onClick={toggleSidebar}>
-              <IoMdMenu className="text-3xl sm:text-3xl" />
-            </div>
+        <div className="md:flex items-center justify-between">
+          <div className="flex items-center justify-between gap-1 sm:gap-3">
+            <button className="md:hidden text-red-600" onClick={toggleSidebar}>
+              <BiMenuAltLeft size={36} />
+            </button>
             <Link to={"/"}>
               <img
-                className="w-24 sm:w-28 md:w-36"
+                className="w-36 sm:w-28 md:w-36"
                 src="https://img.d4040p.com/dp/h5/assets/images/logo.png?v=1735034317574"
                 alt=""
               />
             </Link>
+            <div className="flex items-center gap-3 text-red-600 md:hidden">
+              <div className="flex flex-col items-center cursor-pointer">
+                <LuHardDriveDownload size={26} />
+                <p>app</p>
+              </div>
+              <div className="flex flex-col items-center cursor-pointer">
+                <MdOutlineMessage size={26} />
+                <p>LiveChat</p>
+              </div>
+            </div>
           </div>
-          <div className="flex gap-1 sm:gap-2 md:gap-3">
+          <div className="hidden md:flex gap-1 sm:gap-2 md:gap-3">
             <div className="flex gap-1 sm:gap-2">
               <button
                 className="text-xs sm:text-sm font-medium px-2 sm:px-4 md:px-7 py-1 md:py-2 text-white loginButtonBgColor transition-all duration-300 rounded-md"
@@ -102,282 +110,36 @@ const Navbar = ({ open }) => {
         </div>
       </div>
 
-      {/* Mobile Menu */}
-      <div
-        className={`fixed inset-0 w-full h-screen overflow-y-auto backdrop-blur bg-black/40 z-30 md:hidden transform transition-transform duration-500 ${
-          isSidebarOpen ? "translate-x-0" : "-translate-x-full"
-        }`}
-      >
-        <div className="flex justify-between">
-          <div className="m-2 mb-6 mt-6 w-3/5">
-            <div className="border-2 border-[#2a2a2a] rounded-lg">
-              <div className="flex items-center bg-[#242424] rounded-tl-lg rounded-tr-lg">
-                <img
-                  className="w-24"
-                  src="https://img.d4040p.com/dp/h5/assets/images/member-logo.png?v=1735034317574"
-                  alt=""
-                />
-                <p className="text-sm font-semibold text-yellow-300">
-                  হাই স্বাগতম
-                </p>
-              </div>
-              <div className="bg-yellow-300 py-4 px-4 flex gap-2 justify-between rounded-br-lg rounded-bl-lg">
-                <div className="flex items-center gap-1">
-                  <HiOutlineLogin />
-                  <p className="text-sm font-medium text-black">লগ ইন</p>
-                </div>
-                <div className="flex items-center gap-1">
-                  <BiUserPlus />
-                  <p className="text-sm font-medium text-black">সাইন আপ</p>
-                </div>
-              </div>
-            </div>
-            <div className="mt-2 grid grid-cols-3 gap-x-2 gap-y-[10px]">
-              <Link>
-                <div className="py-2 bg-[#242424] border-2 border-[#2a2a2a] rounded-lg">
-                  <img
-                    className="w-7 m-auto"
-                    src="https://img.m156b.com/mb/h5/assets/images/dark/menu/icon-sport.svg"
-                    alt=""
-                  />
-                  <p className="mt-1 text-white text-[10px] font-medium text-center">
-                    স্পোর্ট
-                  </p>
-                </div>
-              </Link>
-              <Link>
-                <div className="py-2 bg-[#242424] border-2 border-[#2a2a2a] rounded-lg">
-                  <img
-                    className="w-7 m-auto"
-                    src="https://img.m156b.com/mb/h5/assets/images/dark/menu/icon-casino.svg"
-                    alt=""
-                  />
-                  <p className="mt-1 whitespace-nowrap text-white text-[10px] font-medium text-center">
-                    লাইভ ক্যাসিনো
-                  </p>
-                </div>
-              </Link>
-              <Link>
-                <div className="py-2 bg-[#242424] border-2 border-[#2a2a2a] rounded-lg">
-                  <img
-                    className="w-7 m-auto"
-                    src="https://img.m156b.com/mb/h5/assets/images/dark/menu/icon-slot.svg"
-                    alt=""
-                  />
-                  <p className="mt-1 text-white text-[10px] font-medium text-center">
-                    স্লট
-                  </p>
-                </div>
-              </Link>
-              <Link>
-                <div className="py-2 bg-[#242424] border-2 border-[#2a2a2a] rounded-lg">
-                  <img
-                    className="w-7 m-auto"
-                    src="https://img.m156b.com/mb/h5/assets/images/dark/menu/icon-table.svg"
-                    alt=""
-                  />
-                  <p className="mt-1 text-white text-[10px] font-medium text-center">
-                    টেবিল
-                  </p>
-                </div>
-              </Link>
-              <Link>
-                <div className="py-2 bg-[#242424] border-2 border-[#2a2a2a] rounded-lg">
-                  <img
-                    className="w-7 m-auto"
-                    src="https://img.m156b.com/mb/h5/assets/images/dark/menu/icon-crash.svg"
-                    alt=""
-                  />
-                  <p className="mt-1 text-white text-[10px] font-medium text-center">
-                    ক্রাশ
-                  </p>
-                </div>
-              </Link>
-              <Link>
-                <div className="py-2 bg-[#242424] border-2 border-[#2a2a2a] rounded-lg">
-                  <img
-                    className="w-7 m-auto"
-                    src="https://img.m156b.com/mb/h5/assets/images/dark/menu/icon-lottery.svg"
-                    alt=""
-                  />
-                  <p className="mt-1 text-white text-[10px] font-medium text-center">
-                    লটারি
-                  </p>
-                </div>
-              </Link>
-              <Link>
-                <div className="py-2 bg-[#242424] border-2 border-[#2a2a2a] rounded-lg">
-                  <img
-                    className="w-7 m-auto"
-                    src="https://img.m156b.com/mb/h5/assets/images/dark/menu/icon-fish.svg"
-                    alt=""
-                  />
-                  <p className="mt-1 text-white text-[10px] font-medium text-center">
-                    ফিশিং
-                  </p>
-                </div>
-              </Link>
-              <Link>
-                <div className="py-2 bg-[#242424] border-2 border-[#2a2a2a] rounded-lg">
-                  <img
-                    className="w-7 m-auto"
-                    src="https://img.m156b.com/mb/h5/assets/images/dark/menu/icon-arcade.svg"
-                    alt=""
-                  />
-                  <p className="mt-1 text-white text-[10px] font-medium text-center">
-                    আর্কেড
-                  </p>
-                </div>
-              </Link>
-              <Link>
-                <div className="py-2 bg-[#242424] border-2 border-[#2a2a2a] rounded-lg">
-                  <img
-                    className="w-7 m-auto"
-                    src="https://img.m156b.com/mb/h5/assets/images/dark/menu/icon-cockfighting.svg"
-                    alt=""
-                  />
-                  <p className="mt-1 text-white text-[10px] font-medium text-center">
-                    মোরগ লড়াই
-                  </p>
-                </div>
-              </Link>
-            </div>
-            <div className="mt-2 bg-[#242424] border-2 border-[#2a2a2a] rounded-lg">
-              <div className="grid grid-cols-3 items-center">
-                <Link>
-                  <div className="py-2">
-                    <img
-                      className="w-7 m-auto"
-                      src="https://img.m156b.com/mb/h5/assets/images/dark/menu/icon-promotion.svg"
-                      alt=""
-                    />
-                    <p className="mt-1 text-white text-[10px] font-medium text-center">
-                      প্রমোশন
-                    </p>
-                  </div>
-                </Link>
-                <Link>
-                  <div className="py-2">
-                    <img
-                      className="w-7 m-auto"
-                      src="https://img.m156b.com/mb/h5/assets/images/dark/menu/icon-download.svg"
-                      alt=""
-                    />
-                    <p className="mt-1 text-white text-[10px] font-medium text-center">
-                      ডাউনলোড
-                    </p>
-                  </div>
-                </Link>
-                <Link>
-                  <div className="py-2">
-                    <img
-                      className="w-7 m-auto"
-                      src="https://img.m156b.com/mb/h5/assets/images/dark/menu/icon-affiliate.svg"
-                      alt=""
-                    />
-                    <p className="mt-1 text-white text-[10px] font-medium text-center">
-                      অ্যাফিলিয়েট
-                    </p>
-                  </div>
-                </Link>
-                <Link>
-                  <div className="py-2">
-                    <img
-                      className="w-7 m-auto"
-                      src="https://img.m156b.com/mb/h5/assets/images/dark/menu/icon-ambassador.svg"
-                      alt=""
-                    />
-                    <p className="mt-1 text-white text-[10px] font-medium text-center">
-                      অ্যাম্বাসেডর
-                    </p>
-                  </div>
-                </Link>
-              </div>
-            </div>
-            <div className="mt-2 bg-[#242424] border-2 border-[#2a2a2a] rounded-lg">
-              <div className="grid grid-cols-3 items-center">
-                <Link>
-                  <div className="py-2">
-                    <img
-                      className="w-7 m-auto"
-                      src="https://img.m156b.com/mb/h5/assets/images/dark/menu/icon-telegram.svg"
-                      alt=""
-                    />
-                    <p className="mt-1 text-white text-[10px] font-medium text-center">
-                      Telegram
-                    </p>
-                  </div>
-                </Link>
-                <Link>
-                  <div className="py-2">
-                    <img
-                      className="w-7 m-auto"
-                      src="https://img.m156b.com/mb/h5/assets/images/dark/menu/icon-whatsapp.svg"
-                      alt=""
-                    />
-                    <p className="mt-1 text-white text-[10px] font-medium text-center">
-                      WhatsApp
-                    </p>
-                  </div>
-                </Link>
-                <Link>
-                  <div className="py-2">
-                    <img
-                      className="w-7 m-auto"
-                      src="https://img.m156b.com/mb/h5/assets/images/dark/menu/icon-facebook-messenger.svg"
-                      alt=""
-                    />
-                    <p className="mt-1 text-white text-[10px] font-medium text-center">
-                      Facebook Messenger
-                    </p>
-                  </div>
-                </Link>
-                <Link>
-                  <div className="py-2">
-                    <img
-                      className="w-7 m-auto"
-                      src="https://img.m156b.com/mb/h5/assets/images/dark/menu/icon-email.svg"
-                      alt=""
-                    />
-                    <p className="mt-1 text-white text-[10px] font-medium text-center">
-                      মার্কেটিং ইমেইল
-                    </p>
-                  </div>
-                </Link>
-                <Link>
-                  <div className="py-2">
-                    <img
-                      className="w-7 m-auto"
-                      src="https://img.m156b.com/mb/h5/assets/images/dark/menu/icon-email.svg"
-                      alt=""
-                    />
-                    <p className="mt-1 text-white text-[10px] font-medium text-center">
-                      গ্রাহক সাহায্য ইমেল
-                    </p>
-                  </div>
-                </Link>
-              </div>
-            </div>
-            <div className="mt-2 bg-[#242424] border-2 border-[#2a2a2a] rounded-lg grid grid-cols-2">
-              <Link to={"/"}>
-                <div className="flex items-center gap-1 py-4 px-2">
-                  <MdHome size={30} className="text-yellow-400" />
-                  <p className="text-xs font-medium text-white">হোম</p>
-                </div>
-              </Link>
-              <div className="flex items-center gap-1 py-4 px-2">
-                <HiOutlineLogin size={30} className="text-yellow-400" />
-                <p className="text-xs font-medium text-white">লগ ইন</p>
-              </div>
-            </div>
-          </div>
-          <div
-            className="text-white cursor-pointer mt-1"
-            onClick={closeSidebar}
-          >
-            <IoClose size={36} />
-          </div>
-        </div>
+      {/* Mobile Menu login and sign up*/}
+      <div className="fixed bottom-0 left-0 z-50 w-full text-white flex justify-between md:hidden">
+        {/* Bangladesh Flag Section */}
+        <button
+          className="py-1.5 px-1 w-full flex justify-center gap-1 languageBgColor"
+          onClick={toggleLanguageModal}
+        >
+          <img
+            className="w-8 h-8"
+            src="https://png.pngtree.com/png-vector/20220606/ourmid/pngtree-bangladesh-flag-icon-in-modern-neomorphism-style-png-image_4872074.png"
+            alt="BD flag"
+          />
+          <span className="text-sm text-start font-semibold leading-none">
+            BDT <br /> English
+          </span>
+        </button>
+        {/* Sign In Button */}
+        <button
+          className="py-1.5 px-1 w-full flex items-center justify-center signinButtonBgColor"
+          onClick={() => openModal("signup_modal")}
+        >
+          Sign up
+        </button>
+        {/* Login Button */}
+        <button
+          className="py-1.5 px-1 w-full flex items-center justify-center loginButtonBgColor"
+          onClick={() => openModal("login_modal")}
+        >
+          Login
+        </button>
       </div>
 
       {/* Login Modal */}
