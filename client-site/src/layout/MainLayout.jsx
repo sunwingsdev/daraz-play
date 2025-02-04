6d317847393fa6f3ad42a19a9b0e20abef51d12b
vismodeb <vismodeb2000@gmail.com>
2025-02-04 14:10:15 +0600

    Menu problem solv

--- client-site/src/layout/MainLayout.jsx
+++ client-site/src/layout/MainLayout.jsx
@@ -23,10 +23,422 @@ const MainLayout = () => {
 
   // const hideCommonComponents = isMobile && location.pathname === "/register";
 
+  const menuItems = [
+    {
+      name: "Home",
+      icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/theme-icon/icon-home.png?v=1737700451320",
+      path: "/",
+      submenu: [],
+    },
+    {
+      name: "Hot Games",
+      icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/theme-icon/icon-hotgame.png?v=1737700451320",
+      submenu: [
+        {
+          name: "9WICKETS",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/sports-icon/icon-9w.png?v=1735560346274",
+        },
+        {
+          name: "CRAZYTIME",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-crazytime.png?v=1735560346274",
+        },
+        {
+          name: "AVIATOR",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-aviator.png?v=1735560346274",
+          demo: "/games/demo/aviator",
+        },
+        {
+          name: "SUPERACE",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-superace.png?v=1735560346274",
+          demo: "/games/demo/super-ace",
+        },
+        {
+          name: "MONEY COMING",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-moneycoming.png?v=1735560346274",
+        },
+        {
+          name: "ANDAR BAHAR",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-andarbahar.png?v=1735560346274",
+        },
+        {
+          name: "SEXY BACCARAT",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-sexybacarratclassic.png?v=1735560346274",
+        },
+        {
+          name: "7UP7DOWN",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-7up7down2.png?v=1735560346274",
+        },
+      ],
+    },
+    {
+      name: "Sports",
+      icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/theme-icon/icon-sport.png?v=1737700451320",
+      submenu: [
+        {
+          name: "9WICKETS",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/sports-icon/icon-exchange.png?v=1735560346274",
+        },
+        {
+          name: "SBO",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/sports-icon/icon-sbov2.png?v=1735560346274",
+        },
+        {
+          name: "SABA",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-saba.png?v=1735560346274",
+        },
+        {
+          name: "CMD",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/sports-icon/icon-cmd.png?v=1735560346274",
+        },
+        {
+          name: "BTI",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/sports-icon/icon-sbtech.png?v=1735560346274",
+        },
+        {
+          name: "HORSE",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/sports-icon/icon-horsebook.png?v=1735560346274",
+        },
+        {
+          name: "SV388",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/sports-icon/icon-awcmrwb.png?v=1735560346274",
+        },
+        {
+          name: "RWB",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/sports-icon/icon-nst.png?v=1735560346274",
+        },
+        {
+          name: "PINNACLE",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/sports-icon/icon-awcmpinnacle.png?v=1735560346274",
+        },
+      ],
+    },
+    {
+      name: "Slot",
+      icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/theme-icon/icon-slot.png?v=1737700451320",
+      submenu: [
+        {
+          name: "JILI",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmjili.png?v=1735560346274",
+        },
+        {
+          name: "PG",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-pg.png?v=1735560346274",
+        },
+        {
+          name: "JDB",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-jdb.png?v=1735560346274",
+        },
+        {
+          name: "FASTSPIN",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmfastspin.png?v=1735560346274",
+        },
+        {
+          name: "PLAY8",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmp8.png?v=1735560346274",
+        },
+        {
+          name: "REDTIGER",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmrt.png?v=1735560346274",
+        },
+        {
+          name: "SG",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmsg.png?v=1735560346274",
+        },
+        {
+          name: "CQ9",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-cq9.png?v=1735560346274",
+        },
+        {
+          name: "FC",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmfc.png?v=1735560346274",
+        },
+        {
+          name: "KA",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-ka.png?v=1735560346274",
+        },
+        {
+          name: "PP",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmpp.png?v=1735560346274",
+        },
+        {
+          name: "PT",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmpt.png?v=1735560346274",
+        },
+        {
+          name: "NETENT",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-netent.png?v=1735560346274",
+        },
+        {
+          name: "JOKER",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-joker.png?v=1735560346274",
+        },
+        {
+          name: "PNG",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-playngo.png?v=1735560346274",
+        },
+        {
+          name: "NEXTSPIN",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-nextspin.png?v=1735560346274",
+        },
+        {
+          name: "RICH88",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-rich88.png?v=1735560346274",
+        },
+        {
+          name: "WORLDMATCH",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-worldmatch.png?v=1735560346274",
+        },
+        {
+          name: "YELLOWBAT",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmyesbingo.png?v=1735560346274",
+        },
+      ],
+    },
+    {
+      name: "Crash",
+      icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/theme-icon/icon-crash.png?v=1737700451320",
+      submenu: [
+        {
+          name: "AVIATOR",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-jdbaspribe.png?v=1735560346274",
+        },
+        {
+          name: "JILI",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmjili.png?v=1735560346274",
+        },
+        {
+          name: "KM",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmkm.png?v=1735560346274",
+        },
+        {
+          name: "PP",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmpp.png?v=1735560346274",
+        },
+      ],
+    },
+    {
+      name: "Casino",
+      icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/theme-icon/icon-casino.png?v=1737700451320",
+      submenu: [
+        {
+          name: "EVO",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-evo.png?v=1735560346274",
+        },
+        {
+          name: "SEXY",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmsexy.png?v=1735560346274",
+        },
+        {
+          name: "PP",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmpp.png?v=1735560346274",
+        },
+        {
+          name: "PT",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmpt.png?v=1735560346274",
+        },
+        {
+          name: "HOTROAD",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmhotroad.png?v=1735560346274",
+        },
+        {
+          name: "DG",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmdg.png?v=1735560346274",
+        },
+      ],
+    },
+    {
+      name: "Table",
+      icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/theme-icon/icon-table.png?v=1737700451320",
+      submenu: [
+        {
+          name: "JILI",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmjili.png?v=1735560346274",
+        },
+        {
+          name: "KM",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmkm.png?v=1735560346274",
+        },
+        {
+          name: "RICH88",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-rich88.png?v=1735560346274",
+        },
+        {
+          name: "SPRIBE",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-jdbaspribe.png?v=1735560346274",
+        },
+        {
+          name: "PG",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-pg.png?v=1735560346274",
+        },
+        {
+          name: "WORLDMATCH",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-worldmatch.png?v=1735560346274",
+        },
+        {
+          name: "KA",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-ka.png?v=1735560346274",
+        },
+        {
+          name: "CQ9",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-cq9.png?v=1735560346274",
+        },
+        {
+          name: "PNG",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-cq9.png?v=1735560346274",
+        },
+        {
+          name: "BPOKER",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-bpoker.png?v=1735560346274",
+        },
+        {
+          name: "MONOPOLY",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-monopoly.png?v=1735560346274",
+        },
+      ],
+    },
+    {
+      name: "Lottery",
+      icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/theme-icon/icon-lottery.png?v=1737700451320",
+      submenu: [
+        {
+          name: "AVIATOR",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-jdbaspribe.png?v=1735560346274",
+        },
+        {
+          name: "JILI",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmjili.png?v=1735560346274",
+        },
+        {
+          name: "KM",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmkm.png?v=1735560346274",
+        },
+        {
+          name: "PP",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmpp.png?v=1735560346274",
+        },
+      ],
+    },
+    {
+      name: "Fishing",
+      icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/theme-icon/icon-fish.png?v=1737700451320",
+      submenu: [
+        {
+          name: "EVO",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-evo.png?v=1735560346274",
+        },
+        {
+          name: "SEXY",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmsexy.png?v=1735560346274",
+        },
+        {
+          name: "PP",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmpp.png?v=1735560346274",
+        },
+        {
+          name: "PT",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmpt.png?v=1735560346274",
+        },
+        {
+          name: "HOTROAD",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmhotroad.png?v=1735560346274",
+        },
+        {
+          name: "DG",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmdg.png?v=1735560346274",
+        },
+      ],
+    },
+    {
+      name: "Arcade",
+      icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/theme-icon/icon-arcade.png?v=1737700451320",
+      submenu: [
+        {
+          name: "JILI",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmjili.png?v=1735560346274",
+        },
+        {
+          name: "KM",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-awcmkm.png?v=1735560346274",
+        },
+        {
+          name: "RICH88",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-rich88.png?v=1735560346274",
+        },
+        {
+          name: "SPRIBE",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-jdbaspribe.png?v=1735560346274",
+        },
+        {
+          name: "PG",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-pg.png?v=1735560346274",
+        },
+        {
+          name: "WORLDMATCH",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-worldmatch.png?v=1735560346274",
+        },
+        {
+          name: "KA",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-ka.png?v=1735560346274",
+        },
+        {
+          name: "CQ9",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-cq9.png?v=1735560346274",
+        },
+        {
+          name: "PNG",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-cq9.png?v=1735560346274",
+        },
+        {
+          name: "BPOKER",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-bpoker.png?v=1735560346274",
+        },
+        {
+          name: "MONOPOLY",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/brand/white/provider-monopoly.png?v=1735560346274",
+        },
+      ],
+    },
+    {
+      name: "Promotions",
+      icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/theme-icon/icon-promotion.png?v=1737700451320",
+      path: "/promotion",
+    },
+    {
+      name: "Refer Bonus",
+      icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/theme-icon/icon-referral.png?v=1737700451320",
+    },
+    {
+      name: "Download",
+      icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/theme-icon/icon-download.png?v=1737700451320",
+    },
+    {
+      name: "Contact Us",
+      icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/theme-icon/icon-talk.png?v=1737700451320",
+      submenu: [
+        {
+          name: "Telegram Support",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/theme-icon/icon-telegram.png?v=1735560346274",
+        },
+        {
+          name: "Live Chat",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/theme-icon/icon-customer.png?v=1735560346274",
+        },
+        {
+          name: "Messenger",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/theme-icon/icon-facebook-messenger.png?v=1735560346274",
+        },
+        {
+          name: "Email",
+          icon: "https://img.d4040p.com/dp/h5/assets/images/icon-set/theme-icon/icon-email.png?v=1735560346274",
+        },
+      ],
+    },
+  ];
+
   return (
     <div className="flex relative">
       {/* Sidebar */}
-      <SidebarMenu open={open} setOpen={setOpen} />
+      <SidebarMenu open={open} setOpen={setOpen} menuItems={menuItems} />
 
       {/* Content Area */}
       <div
@@ -34,7 +446,7 @@ const MainLayout = () => {
           !open ? "md:pl-16" : "md:pl-64"
         }`}
       >
-        <Navbar open={open} setOpen={setOpen} />
+        <Navbar open={open} setOpen={setOpen} menuItems={menuItems} />
         <div className="mt-[62px] md:mt-16">
           <Outlet />
         </div>
